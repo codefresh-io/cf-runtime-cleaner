e65247a02c1ea5ca1e55001fbea0d382a0254734
Sergey Bahchissaraitsev <sergey@bahchis.com>
2016-01-28 17:31:35 +0200

    per tag handling

--- docker-image-clean.sh
+++ docker-image-clean.sh
@@ -2,18 +2,22 @@
 
 function cleanDockerImages() {
 	echo "Analyzing docker images for cleanup from the last ${CLEAN_PERIOD}..."
-	images=$(docker images | tail -n +2 | cut -f 1 -d ' ')
+	images=$(docker images | tail -n +2 | cut -f 1 -d ' ' | sort | uniq)
 	for image in ${images}; do
 		if [ "${image}" == "<none>" ] || [[ "${image}" == "${PROTECTED_IMAGE_PREFIX}"* ]]; then
 			echo "${image} ignored."
 			continue
 		fi
-		echo "Checking image: ${image} for events..."
-		image_events=$(timeout 5s docker events --filter "image=${image}" --since "${CLEAN_PERIOD}")
-		if [ -z "${image_events}" ]; then
-			echo "deleting ${image}..."
-			docker rmi ${image}
-		fi
+
+		tags=$(docker images ${image} | tail -n +2 | awk -F" " '{print$2}')
+		for tag in ${tags}; do			
+			echo "Checking image: ${image}:${tag} for events..."
+			image_events=$(timeout 5s docker events --filter "image=${image}:${tag}" --since "${CLEAN_PERIOD}")
+			if [ -z "${image_events}" ]; then
+				echo "deleting ${image}:${tag}..."
+				docker rmi ${image}:${tag}
+			fi
+		done
 	done
 
 	# Remove none images
